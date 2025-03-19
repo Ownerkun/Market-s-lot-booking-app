23b4d82ed2cd56076773fed46a2430da5a278b75
Teerawat Ruamsantia <tuiowner2004@gmail.com>
2025-03-19 22:07:08 +0700

    re-design auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:market_lot_app/screen/market_screen/market_list_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
@@ -9,15 +8,9 @@ import 'package:market_lot_app/screen/auth_screen/auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-
-  final keyString = dotenv.env['ENCRYPTION_KEY'] ?? '';
-
-  if (keyString.isEmpty) {
-    throw Exception("Encryption key not found in .env file");
-  }
 
   final prefs = await SharedPreferences.getInstance();
+  final keyString = '12345678901234567890123456789012'; // 32-byte key
   final key = encrypt.Key.fromUtf8(keyString);
   final encrypter = encrypt.Encrypter(encrypt.AES(key));
 
